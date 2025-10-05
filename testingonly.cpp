0c26336886c5d7958ba1891d2f4d8aecc306d8da
Shang <shangwenlim07@gmail.com>
2025-10-05 15:55:03 +0800

    First commit - added project files

---
+++ testingonly.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "Welcome to TARUMT" << endl;
+    cout << "I'am a C++ program" << endl;
+
+    cout << "What is your name?";
+    cin >> name;
+
+    cout << "Hi," << name << ".How are you ? " << endl;
+
+    return 0;
+}
